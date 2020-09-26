174fc7db849dd695f05c3ebe4e41e0dc0cc43e3e
iamsarthakk <sarthakkumar160627@gmail.com>
2020-09-26 21:35:32 +0530

    Updates

---
+++ precache-manifest.e4f93d6af1cf70bc5545dbc9a9433072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c38e86f9d65f7da1d20f8abdea48751",
+    "url": "/ReactWeb/index.html"
+  },
+  {
+    "revision": "e595889cbee21dd89edf",
+    "url": "/ReactWeb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9887a3bb04d91cf247fe",
+    "url": "/ReactWeb/static/js/2.be3e6d08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeb/static/js/2.be3e6d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e595889cbee21dd89edf",
+    "url": "/ReactWeb/static/js/main.726e0bcf.chunk.js"
+  },
+  {
+    "revision": "7245f6aec9d82fcfc78a",
+    "url": "/ReactWeb/static/js/runtime-main.cae74f88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactWeb/static/media/logo.5d5d9eef.svg"
+  }
+]);
