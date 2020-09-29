1bbf68703a4f44fbc9dd809d6bd9e8546c490464
iamsarthakk <sarthakkumar160627@gmail.com>
2020-09-29 20:21:20 +0530

    meh

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from './HeaderComponent';
