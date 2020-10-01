9ecc1237267f7a52e305f295640b4a785afb5c41
iamsarthakk <sarthakkumar160627@gmail.com>
2020-10-01 19:45:14 +0530

    Intro. to Redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+export const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+};
